6ecf8748252fd0ca8dbc6677f473d4742124257d
Jérémie Zarca <jzarca01@users.noreply.github.com>
2018-10-03 10:40:04 +0200

    Wesh alors autoplay

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const { app, BrowserWindow, TouchBar, dialog, shell } = require('electron');
 const { TouchBarButton } = TouchBar;
 
-const WESH_ALORS_URL = 'https://www.youtube.com/watch?v=X6MxGJ7qxck'
+const WESH_ALORS_URL = 'https://www.youtube.com/watch?v=X6MxGJ7qxck?autoplay=1'
 
 const chrisButton = new TouchBarButton({
     icon: path.join(__dirname, '/assets/chris.png'),
